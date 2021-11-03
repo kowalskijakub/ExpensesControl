bf518666d6e0047c4815ebbc882236c598abba67
kowalskijakub <kowalskijakub95@gmail.com>
2021-11-03 15:49:00 +0100

    Create basic registration

--- ExpensesControl/urls.py
+++ ExpensesControl/urls.py
@@ -20,4 +20,5 @@ from mainApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('register/', views.register, name="register"),
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'mainApp/base.html')
+    return render(request, 'mainApp/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'mainApp/register.html', {'form': form})
+
