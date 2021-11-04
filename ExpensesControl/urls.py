5d712448fcabf33cc271f53f9374a7f86f577cf0
kowalskijakub <kowalskijakub95@gmail.com>
2021-11-04 10:38:00 +0100

    Adding login to the website

--- ExpensesControl/urls.py
+++ ExpensesControl/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from mainApp.views import home
-from users.views import register
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
-    path('register/', register, name="register"),
+    path('users/', include('users.urls')),
 ]
