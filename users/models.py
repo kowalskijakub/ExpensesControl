9f1619f002668c4fe3603e97949ee058a66ef964
kowalskijakub <kowalskijakub95@gmail.com>
2021-11-07 16:58:21 +0100

    Adding a profile model. Create signals.py for the user and profile models.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    createDate = models.DateField(default=datetime.date.today)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
