dc769493d33a287525f212526b4c61353b683ed8
kowalskijakub <kowalskijakub95@gmail.com>
2021-11-07 16:10:59 +0100

    Profile.html subpage added

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import register
+from .views import register, profile
 from django.contrib.auth import views as auth_views
 
 urlpatterns =[
     path('register/', register, name="register"),
+    path('profile/', profile, name="profile"),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
