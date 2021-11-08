fa145c8280a90c5bce544dff30e2149437c83f2c
kowalskijakub <kowalskijakub95@gmail.com>
2021-11-08 22:19:55 +0100

    displaying user information in profile.html

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from .forms import UserRegisterForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -21,4 +22,4 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html', {'user': request.user})
+    return render(request, 'users/profile.html', {})
