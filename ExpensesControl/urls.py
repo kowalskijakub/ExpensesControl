53c9a97c3fb1bcbc3b4af3c31e1cfab3d680a08f
kowalskijakub <kowalskijakub95@gmail.com>
2021-11-12 13:44:33 +0100

    Adding the ability to change user details, and uploading a profile picture

--- ExpensesControl/urls.py
+++ ExpensesControl/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from mainApp.views import home
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
     path('users/', include('users.urls')),
     path('wallet/', include('wallet.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import register, profile
+from .views import register, profile, updateProfile
 from django.contrib.auth import views as auth_views
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 urlpatterns =[
     path('register/', register, name="register"),
     path('profile/', profile, name="profile"),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('update/', updateProfile, name="updateProfile"),
+] 
